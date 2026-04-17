422ea3eb3e7e90748d95d215750980639b8f4854
Saheel Mayekar <saheelmayekar@gmail.com>
2026-04-16 21:09:22 -0700

    Added extra chat promtps

--- Week 8/knowledge_cortex/backend/src/rag_engine.py
+++ Week 8/knowledge_cortex/backend/src/rag_engine.py
@@ -32,7 +32,7 @@ def ingest_documents():
         print("No documents found in ./docs.")
         return
         
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
 
     for split in splits:
@@ -81,15 +81,31 @@ def ask_with_transparency(query: str):
     top_3_docs = raw_docs[:3]
     
     # 5. Generate the Answer
+    # prompt = ChatPromptTemplate.from_template("""
+    # Answer the following question based only on the provided context. 
+    # If the answer is not in the context, say "I don't know based on the provided documents". Avoid mentioning that the information was sourced from the context.
+    
+    # Context:
+    # {context}
+    
+    # Question: {input}
+    # """)
+
+    # 5. Generate the Answer
     prompt = ChatPromptTemplate.from_template("""
-    Answer the following question based only on the provided context. 
-    If the answer is not in the context, say "I don't know based on the provided documents."
-    
-    Context:
-    {context}
-    
-    Question: {input}
-    """)
+        Answer the following question based only on the provided context. 
+
+        Guidelines:
+        - If you don't know the answer, simply state that you don't know.
+        - If you're unsure, seek clarification.
+        - Avoid mentioning that the information was sourced from the context.
+        - Respond in accordance with the language of the user's question.
+
+        Context:
+        {context}
+
+        Question: {input}
+        """)
     
     document_chain = create_stuff_documents_chain(llm, prompt)
     answer = document_chain.invoke({"context": top_3_docs, "input": query})
